84a1fb309944c69cdbca693909167e1eebdf4d1b
jirawat malaiwan <tawan.jirawat@gmail.com>
2023-08-05 17:49:45 +0700

    22

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -24,17 +24,5 @@ int main() {
     scanf("%s", lastName1);
     
     printf("%s %ss TC, RMUTL, Chiang Mai, Thailand\n", firstName1, lastName1);
-    
-    char firstName2[50];
-    char lastName2[50];
-
-    printf("First Name: ");
-    scanf("%s", firstName2);
-
-    printf("Last Name: ");
-    scanf("%s", lastName2);
-
-    printf("%s %ss TC, RMUTL, Chiang Mai, Thailand\n", firstName2, lastName2);
-
-    return 0;
-}*/
+   
+}
