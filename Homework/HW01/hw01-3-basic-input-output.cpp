8bf091e6cc15875343d61fa042b0009e071babc1
jirawat malaiwan <tawan.jirawat@gmail.com>
2023-08-05 17:47:15 +0700

    5

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -15,21 +15,48 @@
 */
 #include <stdio.h>
 int main() {
-    char FirstName1[50] ;
-    char LastName1[50] ;
+    char firstName1[50] ;
+    char lastName1[50] ;
     printf("First Name : ") ;
-    scanf( "%s", FirstName1 ) ;
-    printf("Last Name : ") ;
-    scanf("%s", LastName1 ) ;
-    printf("%S %ss TC, RMUTL, Chiang Mai, Thailand\n ", FirstName1, LastName1) ;
+    scanf( "%s", firstName1 ) ;
 
-    char FirstName2[50] ;
-    char LastName2[50] ;
-    printf("First Name : ") ;
-    scanf( "%s", FirstName2 ) ;
     printf("Last Name : ") ;
-    scanf("%s", LastName2 ) ;
-    printf("%S %ss TC, RMUTL, Chiang Mai, Thailand\n ", FirstName2, LastName2) ;
+    scanf("%s", lastName1 ) ;
+
+    printf("%S %ss TC, RMUTL, Chiang Mai, Thailand\n", firstName1, lastName1) ;
+
+    char firstName2[50] ;
+    char lastName2[50] ;
+    printf("First Name : ") ;
+    scanf( "%s", firstName2 ) ;
+    printf("Last Name : ") ;
+    scanf("%s", lastName2 ) ;
+    printf("%S %ss TC, RMUTL, Chiang Mai, Thailand\n", firstName2, lastName2) ;
     
     return 0 ;
-}
+}/*
+#include <stdio.h>
+int main() {
+    char firstName1[50];
+    char lastName1[50];
+    printf("First Name: ");
+    scanf("%s", firstName1);
+
+    printf("Last Name: ");
+    scanf("%s", lastName1);
+    
+    printf("%s %ss TC, RMUTL, Chiang Mai, Thailand\n", firstName1, lastName1);
+    
+    char firstName2[50];
+    char lastName2[50];
+
+    printf("First Name: ");
+    scanf("%s", firstName2);
+
+    printf("Last Name: ");
+    scanf("%s", lastName2);
+
+    printf("%s %ss TC, RMUTL, Chiang Mai, Thailand\n", firstName2, lastName2);
+
+    return 0;
+}*/
